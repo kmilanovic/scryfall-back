c20159ae5c0ec00a31dc14e661e1959b0426daea
kmilanovic <33121923+kmilanovic@users.noreply.github.com>
2023-08-26 21:56:31 +0200

    fix: fix icon files

--- src/main/resources/db/migration/V1_3__create_set_logo_table.sql
+++ src/main/resources/db/migration/V1_3__create_set_logo_table.sql
@@ -10,19 +10,11 @@ VALUES ('40k', 'assets/icon/set-logo/40k.svg');
 INSERT INTO set_icon(name, icon_path)
 VALUES ('Cmm', 'assets/icon/set-logo/cmm.svg');
 INSERT INTO set_icon(name, icon_path)
-VALUES ('Default', 'assets/icon/set-logo/default.svg');
-INSERT INTO set_icon(name, icon_path)
 VALUES ('Ha1', 'assets/icon/set-logo/ha1.svg');
-INSERT INTO set_icon(name, icon_path)
-VALUES ('J22', 'assets/icon/set-logo/j22.svg');
 INSERT INTO set_icon(name, icon_path)
 VALUES ('Ltc', 'assets/icon/set-logo/ltc.svg');
 INSERT INTO set_icon(name, icon_path)
 VALUES ('Ltr', 'assets/icon/set-logo/ltr.svg');
-INSERT INTO set_icon(name, icon_path)
-VALUES ('Mat', 'assets/icon/set-logo/mat.svg');
-INSERT INTO set_icon(name, icon_path)
-VALUES ('Moc', 'assets/icon/set-logo/moc.svg');
 INSERT INTO set_icon(name, icon_path)
 VALUES ('Mom', 'assets/icon/set-logo/mom.svg');
 INSERT INTO set_icon(name, icon_path)
@@ -31,7 +23,3 @@ INSERT INTO set_icon(name, icon_path)
 VALUES ('One', 'assets/icon/set-logo/one.svg');
 INSERT INTO set_icon(name, icon_path)
 VALUES ('Star', 'assets/icon/set-logo/star.svg');
-INSERT INTO set_icon(name, icon_path)
-VALUES ('Unf', 'assets/icon/set-logo/unf.svg');
-INSERT INTO set_icon(name, icon_path)
-VALUES ('Y23', 'assets/icon/set-logo/y23.svg');
